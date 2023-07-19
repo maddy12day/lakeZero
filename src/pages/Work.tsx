a5fa2e0183814866b73f240d346ef819346c3a45
Mohak89 <Mohakagrawal89@gmail.com>
2023-07-20 01:17:38 +0530

    navbar made responsive

--- src/pages/Work.tsx
+++ src/pages/Work.tsx
@@ -1,8 +1,41 @@
 import WorkPageStyle from '../style/WorkPageStyle.module.css'
 export const WorkPage = ()=> {
     return <>
-        <div className={`${WorkPageStyle.Page}`}>
-            Works    
+        <div className={`${WorkPageStyle.Page}`} id='WORK'>
+            <div className={`${WorkPageStyle.CardWrapper}`}>
+            <div className={`${WorkPageStyle.Card}`}>
+                <div className={`${WorkPageStyle.CardImage}`}>
+                    <img src='src/assets/20220213_041556.jpg'/>
+                </div>
+                <span className={`${WorkPageStyle.CardTitle}`}>
+                    Abraca Dabra
+                </span>
+            </div>
+            <div className={`${WorkPageStyle.Card}`}>
+                <div className={`${WorkPageStyle.CardImage}`}>
+                    <img src='src/assets/20220213_041556.jpg'/>
+                </div>
+                <span className={`${WorkPageStyle.CardTitle}`}>
+                    Abraca Dabra
+                </span>
+            </div> 
+            <div className={`${WorkPageStyle.Card}`}>
+                <div className={`${WorkPageStyle.CardImage}`}>
+                    <img src='src/assets/20220213_041556.jpg'/>
+                </div>
+                <span className={`${WorkPageStyle.CardTitle}`}>
+                    Abraca Dabra
+                </span>
+            </div> 
+            <div className={`${WorkPageStyle.Card}`}>
+                <div className={`${WorkPageStyle.CardImage}`}>
+                    <img src='src/assets/20220213_041556.jpg'/>
+                </div>
+                <span className={`${WorkPageStyle.CardTitle}`}>
+                    Abraca Dabra
+                </span>
+            </div> 
+            </div>
         </div>    
     </>
 }
