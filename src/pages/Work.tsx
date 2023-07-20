66da397c7ebbb3be1c45bd4569a3feb28f83ccd3
Mohak89 <Mohakagrawal89@gmail.com>
2023-07-21 00:33:24 +0530

    adding work page

--- src/pages/Work.tsx
+++ src/pages/Work.tsx
@@ -1,41 +1,13 @@
 import WorkPageStyle from '../style/WorkPageStyle.module.css'
-export const WorkPage = ()=> {
+import { RotatingCarousel } from '../components/RotatingCarousel'
+import PageStyle from '../style/Styles.module.css'
+export const WorkPage = () => {
     return <>
-        <div className={`${WorkPageStyle.Page}`} id='WORK'>
-            <div className={`${WorkPageStyle.CardWrapper}`}>
-            <div className={`${WorkPageStyle.Card}`}>
-                <div className={`${WorkPageStyle.CardImage}`}>
-                    <img src='src/assets/20220213_041556.jpg'/>
-                </div>
-                <span className={`${WorkPageStyle.CardTitle}`}>
-                    Abraca Dabra
-                </span>
-            </div>
-            <div className={`${WorkPageStyle.Card}`}>
-                <div className={`${WorkPageStyle.CardImage}`}>
-                    <img src='src/assets/20220213_041556.jpg'/>
-                </div>
-                <span className={`${WorkPageStyle.CardTitle}`}>
-                    Abraca Dabra
-                </span>
-            </div> 
-            <div className={`${WorkPageStyle.Card}`}>
-                <div className={`${WorkPageStyle.CardImage}`}>
-                    <img src='src/assets/20220213_041556.jpg'/>
-                </div>
-                <span className={`${WorkPageStyle.CardTitle}`}>
-                    Abraca Dabra
-                </span>
-            </div> 
-            <div className={`${WorkPageStyle.Card}`}>
-                <div className={`${WorkPageStyle.CardImage}`}>
-                    <img src='src/assets/20220213_041556.jpg'/>
-                </div>
-                <span className={`${WorkPageStyle.CardTitle}`}>
-                    Abraca Dabra
-                </span>
-            </div> 
-            </div>
-        </div>    
+
+        <div className={`${PageStyle.Page} ${WorkPageStyle.Page}`} id='WORK'>
+            <RotatingCarousel />
+            <RotatingCarousel reverse={true} />
+            <RotatingCarousel />
+        </div>
     </>
 }
