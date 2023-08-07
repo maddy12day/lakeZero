6468a78c96e4aa18ec28e7bb04af26ca299b479e
madhavDubey10 <madhav.dubey@aretove.com>
2023-08-07 22:55:09 +0530

    new about page

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,58 +1,34 @@
 import AboutPageStyle from '../style/AboutPageStyle.module.css'
 import aboutData from '../data/aboutData.json'
-import PageStyle from '../style/PageStyle.module.css'
 
 export const AboutPage = () => {
     return <>
-        <div className={`${AboutPageStyle.Page} ${PageStyle.Page}`} id='ABOUT'>
-            <div className={`${PageStyle.ContainerColFlex} ${AboutPageStyle.LeftSide}`}>
-                {/* HEADING */}
-                <div className={`${PageStyle.Text} ${AboutPageStyle.heading}`}>
-                    About
-                </div>
-
-                {/* BULLET POINTS  */}
-
-                <div className={` ${PageStyle.Note} ${AboutPageStyle.points}`}>
-                    {
-                        aboutData.map(items => (
-                            <p>
-                                {items.aboutDescription}
-                            </p>
-                        )
-                        )
-                    }
-                </div>
+        <div className={`${AboutPageStyle.parentDiv}`} id='ABOUT'>
+            <div className={`${AboutPageStyle.imageDiv} ${AboutPageStyle.blurEnable}`}></div>
+            <div className={`${AboutPageStyle.aboutBody}`}>
+                
+                    <section className={`${AboutPageStyle.leftSection}`}>
+                        <h1 className={`${AboutPageStyle.aboutHeading}`}>ABOUT</h1>
+                        <article className={`${AboutPageStyle.aboutDesc}`}>
+                        {aboutData.map(items=>(
+                            <p>{items.aboutDescription}</p>
+                        ))}
+                        </article>
+                    </section>
+                    <section className={`${AboutPageStyle.aboutImg}`}>
+                        <span className={`${AboutPageStyle.aboutOwner}`}>
+                            SUKRIT SHRIVASTAV
+                            <hr />
+                            Composer|Music Producer
+                        </span>
+                        <span className={`${AboutPageStyle.aboutOwner}`}>
+                            ANIMESH SHUKLA
+                            <hr />
+                            Music Producer|Guitarist
+                        </span>
+                    </section>
+                
             </div>
-            <div className={`${PageStyle.ContainerColFlex} ${AboutPageStyle.RightSide}`}>
-                <div className={`${AboutPageStyle.Card}`}>
-                    <div className={`${AboutPageStyle.CardBackground}`}>
-                        <img src='/src/assets/About.jpg'/>
-                    </div>
-                    <div className={`${AboutPageStyle.CardWrapper}`}>
-                    <div className={`${AboutPageStyle.Data}`}>
-                        <span className={`${PageStyle.nameStyle}`}>SUKRIT SHRIVASTAV</span>
-                        <hr/>
-                        <span>Composer | Music Producer</span>
-                        </div>
-                    </div>
-                </div>
-                <div className={`${AboutPageStyle.Card}`}>
-                <div className={`${AboutPageStyle.CardBackground}`}>
-                        <img src='src/assets/about.jpg'/>
-                    </div>
-                    <div className={`${AboutPageStyle.CardWrapper}`}>
-                        <div className={`${AboutPageStyle.Data}`}>
-
-                        <span className={`${AboutPageStyle.nameStyle}`}>ANIMESH SHUKLA</span>
-                        <hr/>
-                        <span>Music Producer | Guitarist</span>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            {/* <div className={`${AboutPageStyle.Circle1}`}></div> */}
-            {/* <div className={`${AboutPageStyle.Circle2}`}></div> */}
         </div>
     </>
 }
